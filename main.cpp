21e8795ea7d8a476b8dbcf13dbe963e46350ac7d
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-01-08 23:59:30 +0800

    Update glfw and glew into deps

--- main.cpp
+++ main.cpp
@@ -1,27 +1,38 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "Failed to initialise GLFW" << std::endl;
         return -1;
+    }
+        
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to open GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glewExperimental=true; // Needed in core profile
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;;
+        return -1;
+    }
     glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
