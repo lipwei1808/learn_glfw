2530eba146e869611ba718a442cebb22ab9a65f0
Tan Lip Wei <53000252+lipwei1808@users.noreply.github.com>
2024-01-15 00:38:46 +0800

    Render second object

--- src/main.cpp
+++ src/main.cpp
@@ -56,10 +56,10 @@ int main(void)
     {
         // An array of 3 vectors which represents 3 vertices
         static const GLfloat g_vertex_buffer_data[] = {
-            000.0f, 300.0f, 0.0f, 0.0f,
-            400.0f, 300.0f, 1.0f, 0.0f,
-            900.0f, 400.0f, 1.0f, 1.0f,
-            100.0f, 400.0f, 0.0f, 1.0f,
+            -50.0f, -50.0f, 0.0f, 0.0f,
+            50.0f, -50.0f, 1.0f, 0.0f,
+            50.0f, 50.0f, 1.0f, 1.0f,
+            -50.0f, 50.0f, 0.0f, 1.0f,
         };
 
         unsigned int indices[] = {
@@ -81,7 +81,7 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(200, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         // Use shader
         Shader shader("res/shaders/Basic.shader");
@@ -110,7 +110,8 @@ int main(void)
 
         float r = 0.0f;
         float increment = 0.05f;
-        glm::vec3 translation(200, -200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window) && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
         {
@@ -118,14 +119,20 @@ int main(void)
             renderer.Clear();
             ImGui_ImplGlfwGL3_NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
-            
-            GLCall(shader.Bind());
-            GLCall(shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f));
-           GLCall(shader.SetUniformMat4f("u_MVP", mvp));
-
-            renderer.Draw(va, ib, shader);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                GLCall(shader.Bind());
+                GLCall(shader.SetUniformMat4f("u_MVP", mvp));
+                renderer.Draw(va, ib, shader);
+            }
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                GLCall(shader.Bind());
+                GLCall(shader.SetUniformMat4f("u_MVP", mvp));
+                renderer.Draw(va, ib, shader);
+            }
 
             if (r > 1.0f) {
                 increment = -0.05f;
@@ -137,7 +144,8 @@ int main(void)
             {
                 static float f = 0.0f;
                 static int counter = 0;
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
